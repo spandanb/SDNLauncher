766c7d6d952335d1ca260a063b27a4cd5892fa6a
Hadi Bannazadeh <hadi.bannazadeh@utoronto.ca>
2014-07-15 17:28:49 -0400

    removing dependency to whale. fixing license

--- cleanup.py
+++ cleanup.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2014 University of Toronto
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # vim: tabstop=4 shiftwidth=4 softtabstop=4 expandtab
 
 import novaclient.v1_1.client as nclient
@@ -12,8 +27,6 @@ from quantumclient.v2_0 import client as qclient
 import smtplib
 from quantumclient.quantum import v2_0 as quantumv20
 from novaclient import exceptions
-import whaleclient
-import whaleclient.exc
 from keystoneclient.v2_0 import client as ksclient
 import re
 
